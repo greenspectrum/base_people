f8e6e39ebd13dd5ee8a7004b512ef24bfba275f4
greenspectrum <service4f@gmail.com>
2018-05-03 22:21:22 +0300

    fixed output

--- entrance.py
+++ entrance.py
@@ -1,12 +1,11 @@
 from tkinter import *
 from module_family import Family
-fam = Family('localhost','family','root','13-qw24')
 
-
+fam = Family('localhost', 'family', 'root', '13-qw24')
 
 root = Tk()
 root.title('Аутентификация')
-root.minsize(220,160)
+root.minsize(220, 160)
 root.resizable(width=False, height=False)
 
 login_text = Label(root, text='Login').place(x=5, y=2, width=60, height=40)
@@ -17,20 +16,21 @@ pass_text = Label(root, text='Password').place(x=5, y=30)
 pass_entry = Entry(root, width=15, show='*')
 pass_entry.place(x=90, y=30)
 
-#=====================================ADMIN==========================================================
+
+# =====================================ADMIN==========================================================
 def admin():
-    #Создаем класс окна
+    # Создаем класс окна
     root_admin = Tk()
     root_admin.title('Моя родословная')
     root_admin.minsize(680, 330)
     root_admin.resizable(width=False, height=False)
 
-    #создаем текстовое окошко для вывода
-    output = Text(root_admin, bg='white', font='Arial 12',wrap=WORD)
+    # создаем текстовое окошко для вывода
+    output = Text(root_admin, bg='white', font='Arial 12', wrap=WORD)
     output.place(x=10, y=115, width=645, height=200)
 
-    scr = Scrollbar(root_admin, command=output.yview)  #создаем скролл бар
-    output.configure(yscrollcommand = scr.set)
+    scr = Scrollbar(root_admin, command=output.yview)  # создаем скролл бар
+    output.configure(yscrollcommand=scr.set)
     scr.place(x=660, y=180)
 
     def show_people():
@@ -67,6 +67,7 @@ def admin():
         a4.grid(row=5, column=2)
         l4 = Label(modal, text='comment')  # заголовок который мы видим
         l4.grid(row=5, column=1)
+
         #   name, surname, dateofbirth, dateofdeath, comment
         # делаем кнопку сохранить запись
         def save_people():
@@ -88,11 +89,13 @@ def admin():
         a.grid(row=1, column=2)
         l = Label(modal_del, text='Введите № записи:')  # заголовок который мы видим
         l.grid(row=1, column=1)
+
         # делаем кнопку сохранить запись
         def deleting_people():
             fam.delete_people(a.get())  # delete_people - берем из модуля module_family.py
             output.insert('0.0', '*Запись удалена\n')
             modal_del.destroy()
+
         button_del_people = Button(modal_del, text='Удалить', width=15, height=1,
                                    command=deleting_people)  # command -это именно метод поэтому без скобок
         button_del_people.grid(row=3, column=2)
@@ -120,7 +123,7 @@ def admin():
         for i in r:
             lis.insert(END, i)
         lis.selection_set(0, 0)
-        if lis.selection_get()=='Administrator':
+        if lis.selection_get() == 'Administrator':
             roots = 1
         else:
             roots = 0
@@ -132,6 +135,7 @@ def admin():
                 a['bg'] = 'red'
             else:
                 a['bg'] = '#7CFC00'
+
         a.bind('<Any-KeyRelease>', correct_login)
 
         def correct_pass(event):
@@ -140,8 +144,8 @@ def admin():
                 a1['bg'] = 'red'
             else:
                 a1['bg'] = '#7CFC00'
-        a1.bind('<Any-KeyRelease>', correct_pass)
 
+        a1.bind('<Any-KeyRelease>', correct_pass)
 
         # делаем кнопку сохранить запись
         def save_user():
@@ -157,12 +161,13 @@ def admin():
                 modal_ok.minsize(200, 90)
                 l = Label(modal_ok, text='Введите корректные данные!')
                 l.grid(row=1, column=1)
+
                 def close_ok():
                     modal_ok.destroy()
-                button_ok = Button(modal_ok, text='Ok', width=15, height=1,
-                                            command=close_ok)  # command -это именно метод поэтому без скобок
-                button_ok.grid(row=3, column=1)
 
+                button_ok = Button(modal_ok, text='Ok', width=15, height=1,
+                                   command=close_ok)  # command -это именно метод поэтому без скобок
+                button_ok.grid(row=3, column=1)
 
         button_save_people = Button(modal, text='Сохранить', width=15, height=1,
                                     command=save_user)  # command -это именно метод поэтому без скобок
@@ -177,63 +182,62 @@ def admin():
         a.grid(row=1, column=2)
         l = Label(modal_del, text='Введите логин пользователя:')  # заголовок который мы видим
         l.grid(row=1, column=1)
+
         # делаем кнопку сохранить запись
         def deleting_user():
             fam.delete_user(a.get())  # delete_user - берем из модуля module_family.py
             output.insert('0.0', '*Запись удалена\n')
             modal_del.destroy()
+
         button_del_people = Button(modal_del, text='Удалить', width=15, height=1,
                                    command=deleting_user)  # command -это именно метод поэтому без скобок
         button_del_people.grid(row=3, column=2)
         modal_del.mainloop()
 
+    # создаем кнопку 'Список людей'
+    but_show_humans = Button(root_admin, text='Список людей', bg='white', fg='black', font='Arial',
+                             width=22, height=1, command=show_people)  # fg - цвет шрифта
+    but_show_humans.place(x=10, y=10)
 
+    # создаем кнопку 'Очистить вывод'
+    but_cls_humans = Button(root_admin, text='Очистить вывод', bg='white', fg='black', font='Arial',
+                            width=22, height=1, command=cls_screen)  # fg - цвет шрифта
+    but_cls_humans.place(x=10, y=50)
 
+    but_add_humans = Button(root_admin, text='Добавить новую запись', bg='white', fg='black', font='Arial',
+                            width=22, height=1, command=add_people)  # fg - цвет шрифта
+    but_add_humans.place(x=230, y=10)
 
-    #создаем кнопку 'Список людей'
-    but_show_humans = Button(root_admin, text = 'Список людей', bg='white', fg='black', font='Arial',
-                              width=22, height=1, command=show_people ) #fg - цвет шрифта
-    but_show_humans.place(x = 10, y = 10)
+    but_del_humans = Button(root_admin, text='Удалить запись', bg='white', fg='black', font='Arial',
+                            width=22, height=1, command=del_people)  # fg - цвет шрифта
+    but_del_humans.place(x=230, y=50)
 
-    #создаем кнопку 'Очистить вывод'
-    but_cls_humans = Button(root_admin, text = 'Очистить вывод', bg='white', fg='black',font='Arial',
-                              width=22, height=1, command=cls_screen) #fg - цвет шрифта
-    but_cls_humans.place(x = 10, y = 50)
+    but_add_user = Button(root_admin, text='Добавить пользователя', bg='white', fg='black', font='Arial',
+                          width=22, height=1, command=add_user)  # fg - цвет шрифта
+    but_add_user.place(x=452, y=10)
 
-
-    but_add_humans = Button(root_admin, text = 'Добавить новую запись', bg='white', fg='black',font='Arial',
-                              width=22, height=1, command=add_people) #fg - цвет шрифта
-    but_add_humans.place(x = 230, y = 10)
-
-
-    but_del_humans = Button(root_admin, text = 'Удалить запись', bg='white', fg='black',font='Arial',
-                              width=22, height=1, command=del_people) #fg - цвет шрифта
-    but_del_humans.place(x = 230, y = 50)
-
-    but_add_user = Button(root_admin, text = 'Добавить пользователя', bg='white', fg='black',font='Arial',
-                              width=22, height=1, command=add_user) #fg - цвет шрифта
-    but_add_user.place(x = 452, y = 10)
-
-    but_del_user = Button(root_admin, text = 'Удалить пользователя', bg='white', fg='black',font='Arial',
-                              width=22, height=1, command=del_user) #fg - цвет шрифта
-    but_del_user.place(x = 452, y = 50)
+    but_del_user = Button(root_admin, text='Удалить пользователя', bg='white', fg='black', font='Arial',
+                          width=22, height=1, command=del_user)  # fg - цвет шрифта
+    but_del_user.place(x=452, y=50)
 
     root_admin.mainloop()
-#=====================================USER==========================================================
+
+
+# =====================================USER==========================================================
 
 def user():
-    #Создаем класс окна
+    # Создаем класс окна
     root_admin = Tk()
     root_admin.title('Моя родословная')
     root_admin.minsize(680, 330)
     root_admin.resizable(width=False, height=False)
 
-    #создаем текстовое окошко для вывода
-    output = Text(root_admin, bg='white', font='Arial 12',wrap=WORD)
+    # создаем текстовое окошко для вывода
+    output = Text(root_admin, bg='white', font='Arial 12', wrap=WORD)
     output.place(x=10, y=115, width=645, height=200)
 
-    scr = Scrollbar(root_admin, command=output.yview)  #создаем скролл бар
-    output.configure(yscrollcommand = scr.set)
+    scr = Scrollbar(root_admin, command=output.yview)  # создаем скролл бар
+    output.configure(yscrollcommand=scr.set)
     scr.place(x=660, y=180)
 
     def show_people():
@@ -245,19 +249,20 @@ def user():
     def cls_screen():
         output.delete(1.0, END)
 
-    #создаем кнопку 'Список людей'
-    but_show_humans = Button(root_admin, text = 'Список людей', bg='white', fg='black', font='Arial',
-                              width=22, height=1, command=show_people ) #fg - цвет шрифта
-    but_show_humans.place(x = 10, y = 10)
+    # создаем кнопку 'Список людей'
+    but_show_humans = Button(root_admin, text='Список людей', bg='white', fg='black', font='Arial',
+                             width=22, height=1, command=show_people)  # fg - цвет шрифта
+    but_show_humans.place(x=10, y=10)
 
-    #создаем кнопку 'Очистить вывод'
-    but_cls_humans = Button(root_admin, text = 'Очистить вывод', bg='white', fg='black',font='Arial',
-                              width=22, height=1, command=cls_screen) #fg - цвет шрифта
-    but_cls_humans.place(x = 10, y = 50)
+    # создаем кнопку 'Очистить вывод'
+    but_cls_humans = Button(root_admin, text='Очистить вывод', bg='white', fg='black', font='Arial',
+                            width=22, height=1, command=cls_screen)  # fg - цвет шрифта
+    but_cls_humans.place(x=10, y=50)
 
     root_admin.mainloop()
 
-#==================================================================================================
+
+# ==================================================================================================
 
 
 def playto():
@@ -295,27 +300,30 @@ def playto():
         out.insert("0.0", "ERROR!: Вы ввели не верные данные.")
         rooter.mainloop()
 
+
 Button(root, text='Sign in', command=playto).place(x=60, y=70, width=90, height=40)
-b= Button(root, text='Admin', command=admin).place(x=60, y=140, width=90, height=40)
+b = Button(root, text='Admin', command=admin).place(x=60, y=140, width=90, height=40)
 
 
 def correct_login(event):
     data = login_entry.get()
-    if len(data)>10 or len(data)<4:
-        login_entry['bg']='red'
+    if len(data) > 10 or len(data) < 4:
+        login_entry['bg'] = 'red'
     else:
-        login_entry['bg']='#7CFC00'
+        login_entry['bg'] = '#7CFC00'
+
+
 login_entry.bind('<Any-KeyRelease>', correct_login)
 
 
 def correct_pass(event):
     data = pass_entry.get()
-    if len(data)<3:
-        pass_entry['bg']='red'
+    if len(data) < 3:
+        pass_entry['bg'] = 'red'
     else:
-        pass_entry['bg']='#7CFC00'
+        pass_entry['bg'] = '#7CFC00'
+
+
 pass_entry.bind('<Any-KeyRelease>', correct_pass)
 
 root.mainloop()
-
-
--- module_family.py
+++ module_family.py
@@ -2,7 +2,6 @@ import mysql.connector
 from mysql.connector import Error
 
 
-
 class Family:
     def __init__(self, host, database, user, password):
         self.host = host
@@ -17,30 +16,27 @@ class Family:
         except Error as e:
             print(e)
 
-
     def get_conn(self):
-        conn = mysql.connector.connect(host = self.host,
-                                           database = self.database,
-                                           user = self.user,
-                                           password = self.password)
+        conn = mysql.connector.connect(host=self.host,
+                                       database=self.database,
+                                       user=self.user,
+                                       password=self.password)
         return conn
-
 
     def print_people(self):
         try:
             sql = 'SELECT * from people'
             conn = self.get_conn()
-            c = conn.cursor()       #получаем в курсор
-            c.execute(sql)           # извлекаем из курсора
+            c = conn.cursor()  # получаем в курсор
+            c.execute(sql)  # извлекаем из курсора
             print('Список всех людей в базе:')
-            rows = c.fetchall()     #метод достать все
+            rows = c.fetchall()  # метод достать все
             for row in rows:
                 print(row)
             conn.close()
             return rows
         except Error as e:
             print(e)
-
 
     def add_people(self, name, surname, date_of_birth, date_of_death, comment):
         try:
@@ -51,7 +47,7 @@ class Family:
             c = conn.cursor()  # получаем в курсор
             c.execute(sql, args)
             # c.execute(sql)
-            conn.commit()   # заносится в базу только когда коммит
+            conn.commit()  # заносится в базу только когда коммит
             conn.close()
         except Error as e:
             print(e)
@@ -67,6 +63,7 @@ class Family:
                     new_simbol = chr(new_code)
                     new_message.append(new_simbol)
                 return ''.join(new_message)
+
             password_encode = cezar_encode(9, password)
 
             sql = 'INSERT INTO logins (login, password, root) VALUES (%s, %s, %s)'
@@ -75,15 +72,14 @@ class Family:
             c = conn.cursor()  # получаем в курсор
             c.execute(sql, args)
             # c.execute(sql)
-            conn.commit()   # заносится в базу только когда коммит
+            conn.commit()  # заносится в базу только когда коммит
             conn.close()
         except Error as e:
             print(e)
 
-
     def update_name(self, id, name):
         try:
-            #sql = 'INSERT INTO category (id, name, discount, alias_name) VALUES (%s, %s, %s, %s)' если делать через format - то СТРОКИ всегда в кавычках {}
+            # sql = 'INSERT INTO category (id, name, discount, alias_name) VALUES (%s, %s, %s, %s)' если делать через format - то СТРОКИ всегда в кавычках {}
 
             sql = 'UPDATE people SET name = %s WHERE id = %s'
             args = (name, id)
@@ -91,12 +87,10 @@ class Family:
             c = conn.cursor()  # получаем в курсор
             c.execute(sql, args)
             # c.execute(sql)
-            conn.commit()   # заносится в базу только когда коммит
+            conn.commit()  # заносится в базу только когда коммит
             conn.close()
         except Error as e:
             print(e)
-
-
 
     def delete_people(self, id):
         try:
@@ -104,7 +98,7 @@ class Family:
             conn = self.get_conn()
             c = conn.cursor()  # получаем в курсор
             c.execute(sql, (id,))  # СТАВИМ ЗАПЯТУЮ КОГДА ОДИН ПАРАМЕТР
-            conn.commit()   # заносится в базу только когда коммит
+            conn.commit()  # заносится в базу только когда коммит
             conn.close()
         except Error as e:
             print(e)
@@ -120,10 +114,11 @@ class Family:
         except Error as e:
             print(e)
 
-# функция авторизации - для входа в админку
+        # функция авторизации - для входа в админку
+
     def aft(self, log, pas):
 
-        #Функция декодирования пароля
+        # Функция декодирования пароля
         def cezar_encode(key, message):
             new_message = []
             for letter in message:
@@ -133,30 +128,30 @@ class Family:
                 new_message.append(new_simbol)
             return ''.join(new_message)
 
-        password_decode = cezar_encode(9,pas)
-        print('new:'+ pas +' old'+ password_decode)
+        password_decode = cezar_encode(9, pas)
+        print('new:' + pas + ' old' + password_decode)
 
         try:
             sql = "SELECT root FROM family.logins WHERE login = %s and password = %s"
             conn = mysql.connector.connect(host=self.host,
-                                       database=self.database,
-                                       user=self.user,
-                                       password=self.password)
+                                           database=self.database,
+                                           user=self.user,
+                                           password=self.password)
             c = conn.cursor()
-            c.execute(sql,(log,password_decode))
+            c.execute(sql, (log, password_decode))
             rows = c.fetchall()
             if (rows == []):
-                rows = ['e','r','r','o','r']
+                rows = ['e', 'r', 'r', 'o', 'r']
             return rows[0]
         except Error as e:
             return e
 
-if __name__ == "__main__":
 
-    fam = Family('localhost','family','root','13-qw24')
+if __name__ == "__main__":
+    fam = Family('localhost', 'family', 'root', '13-qw24')
     fam.print_people()
 
-#    fam.update_name('1','Паша')
+    #    fam.update_name('1','Паша')
     fam.delete_people('6')
     fam.delete_people('7')
     fam.add_people('Андрей', 'Матье', '1988', '1765', 'просто хороший человек')
